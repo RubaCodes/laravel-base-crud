0274cd82d1c9bb4ec41ab4523565dc2dd97d8f4e
RubaCodes <rubacodes@gmail.com>
2022-07-14 16:23:10 +0200

    fix stili

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,10 +8,10 @@ Comics
 <h1 class="text-center">Elenco Comics DC</h1>
 <div class="row row-cols-4 border">
     @foreach ($comics as $comic)
-        <div class="col text-center border p-4 bg-danger">
+        <div class="col text-center border p-4 bg-black">
             <img  src="{{$comic->thumb}}" alt="{{$comic->title}}">
-            <a href="{{ route('comics.show', $comic->id) }}">
-                <h3 class="text-white">{{$comic->title}}</h3>
+            <a class="link-info" href="{{ route('comics.show', $comic->id) }}">
+                <h3 >{{$comic->title}}</h3>
             </a>
         </div>
     @endforeach
