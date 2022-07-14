3eab9659b62f9095b64789d1dcffbcd82172b8c6
RubaCodes <rubacodes@gmail.com>
2022-07-14 15:58:06 +0200

    style libreria comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,5 +5,13 @@ Comics
 @endsection
 
 @section('page-content')
-{{$comics}}
+<h1 class="text-center">Elenco Comics DC</h1>
+<div class="row row-cols-4 border">
+    @foreach ($comics as $comic)
+        <div class="col text-center border p-4 bg-danger">
+            <img  src="{{$comic->thumb}}" alt="{{$comic->title}}">
+            <h3 class="text-white">{{$comic->title}}</h2>
+        </div>
+    @endforeach
+  </div>
 @endsection
