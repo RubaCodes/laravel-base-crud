d5ce9ea34a4f75ead3d89edb153465583a6d01bc
RubaCodes <rubacodes@gmail.com>
2022-07-14 19:00:42 +0200

    finx numer non funzionante

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,7 +10,7 @@
     @csrf
     <div class="mb-3">
       <label for="titolo" class="form-label">Titolo del fumetto</label>
-      <input name="title" type="text" class="form-control" id="titolo" aria-describedby="emailHelp">
+      <input name="title" type="text" class="form-control" id="titolo" >
     </div>
     <label class="mb-3" for="descrizione">Descrizione del Fumetto</label>
     <div class="form-floating mb-3">
@@ -18,23 +18,23 @@
     </div>
     <div class="mb-3">
         <label for="serie" class="form-label">Serie</label>
-        <input name="series" type="text" class="form-control" id="serie" aria-describedby="emailHelp">
+        <input name="series" type="text" class="form-control" id="serie" >
     </div>
     <div class="mb-3">
         <label for="tipo" class="form-label">Tipo</label>
-        <input name="type"type="text" class="form-control" id="tipo" aria-describedby="emailHelp">
+        <input name="type"type="text" class="form-control" id="tipo" >
     </div>
     <div class="mb-3">
         <label for="thumbnail" class="form-label">Path immagine copertina</label>
-        <input name="thumb" type="text" class="form-control" id="thumbnail" aria-describedby="emailHelp">
+        <input name="thumb" type="text" class="form-control" id="thumbnail" >
     </div>
     <div class="mb-3">
         <label for="prezzo" class="form-label">Prezzo</label>
-        <input class="price" type="number" class="form-control" id="prezzo" aria-describedby="emailHelp">
+        <input name="price" type="number" step="any" class="form-control" id="prezzo" >
     </div>
     <div class="mb-3">
         <label for="data" class="form-label">Data di uscita</label>
-        <input name="sale_date" type="date"  class="form-control" id="data" aria-describedby="emailHelp">
+        <input name="sale_date" type="date"  class="form-control" id="data" >
     </div>
 
     <label class="mb-3" for="Scrittori">Scrittori ( separati da una virgola)</label>
