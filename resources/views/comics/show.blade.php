5c16d2d141a50a559d24e21099657282fc14e080
RubaCodes <rubacodes@gmail.com>
2022-07-14 16:17:36 +0200

    rifinito stile

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,16 +5,20 @@
 @endsection
 
 @section('page-content')
-<div class="text-center">
-    <h1>
-        {{$comic->title}}  
-    </h1>
+<div class="text-center py-4">
     <div class="d-flex justify-content-between gap-4">
         <img width="300" src="{{$comic->thumb}}" alt="{{$comic->title}}">
         <div class=" flex-grow-1">
+            <h1>
+                {{$comic->title}}  
+            </h1>
             <p>{{$comic->description}}</p>
-            <p> Scrittori :{{$comic->artists}}</p>
-            <p> Artisti :{{$comic->artists}}</p>
+            <span class="d-block"> Scrittori :{{$comic->artists}}</span>
+            <span class="d-block"> Artisti :{{$comic->artists}}</span>
+            <div class="py-4">
+                <span class="px-2"> Prezzo: {{$comic->price}} $</span>
+                <span class="px-2"> Data di uscita: {{$comic->sale_date}}</span>
+            </div>
         </div>
     </div>
 </div>
